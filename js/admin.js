257c17bd921bf2786a9b02209d78d911543c1092
Eduardo-Nunes <eduardonunes056@gmail.com>
2025-06-05 21:45:02 -0300

    add auth logic

--- js/admin.js
+++ js/admin.js
@@ -1,10 +1,20 @@
+function getObjLoginSenha() {
+  var email = $('#email').val();
+  var senha = $('#senha').val();
+  console.log(email, senha);
+  return {
+    email: email,
+    senha: senha
+  };
+}
+
 $(document).ready(function() {
   $('#adminForm').submit(function(event) {
     event.preventDefault();
-    var email = $('#email').val();
-    var senha = $('#senha').val();
-    // Aqui você pode adicionar a lógica de autenticação
-    console.log('Autenticando:', email, senha);
-    // Exemplo: if (email === 'admin@luxo.com' && senha === 'senha123') { ... }
+    if (validarUsuario(getObjLoginSenha())) {
+      window.location.href = 'mensagens.html';
+    } else {
+      alert('E-mail e Senha inválidos');
+    }
   });
 }); 
